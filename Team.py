084f35f1af5e463aeffaad3727914d95a387e375
James <jnt999@vt.edu>
2017-10-07 21:43:02 -0400

    CreateTeamClass

---
+++ Team.py
@@ -0,0 +1,12 @@
+class Team:
+    #Creates the team and initializes all the players
+    def __init__(self):
+        self._pg = null
+        self._sg = null
+        self._sf = null
+        self._pf = nul
+        self._c = null
+        self._g = null
+        self._util = null
+
+    
